e49de5b2eac76eedb3bd90aa91847e44f1aa241f
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-07-01 01:22:15 +0700

    grabprod

--- app/Http/Controllers/API/LazopController.php
+++ app/Http/Controllers/API/LazopController.php
@@ -107,64 +107,44 @@ class LazopController extends Controller
 
     public function get_product()
     {
+        $tokenwehave = $this->accessToken;
+        $arr = [];
         $method = 'GET';
         $apiName = '/products/get';
 
-        $c = new LazopClient($this->apiGateway, $this->apiKey, $this->apiSecret);
-        $request = new LazopRequest($apiName,$method);
+        foreach ($tokenwehave as $key => $value) {
+            $c = new LazopClient($this->apiGateway, $this->apiKey, $this->apiSecret);
+            $request = new LazopRequest($apiName,$method);
+            $request->addApiParam('filter','live');
+            // $request->addApiParam('update_before','2018-01-01T00:00:00+0800');
+            // $request->addApiParam('create_before','2018-01-01T00:00:00+0800');
+            $request->addApiParam('offset','0');
+            $request->addApiParam('create_after','2010-01-01T00:00:00+0800');
+            // $request->addApiParam('update_after','2010-01-01T00:00:00+0800');
+            $request->addApiParam('limit','50');
+            $request->addApiParam('options','1');
+            // $request->addApiParam('sku_seller_list',' [\"39817:01:01\", \"Apple 6S Black\"]');
+            $executelazop = json_decode($c->execute($request, $value['token']), true);
 
-        $request->addApiParam('filter','live');
-        // $request->addApiParam('update_before','2018-01-01T00:00:00+0800');
-        // $request->addApiParam('create_before','2018-01-01T00:00:00+0800');
-        $request->addApiParam('offset','0');
-        $request->addApiParam('create_after','2010-01-01T00:00:00+0800');
-        // $request->addApiParam('update_after','2010-01-01T00:00:00+0800');
-        $request->addApiParam('limit','50');
-        $request->addApiParam('options','1');
-        // $request->addApiParam('sku_seller_list',' [\"39817:01:01\", \"Apple 6S Black\"]');
+            if (isset($executelazop['data']['products'])) {
 
-        $executelazop = json_decode($c->execute($request, $this->accessToken[0]['token']), true);
-        $data = $executelazop['data']['products'];
-
-        for ($i=0; $i < count($data); $i++) { 
-            $res['data'][$i]['itemid'] = isset($data[$i]['item_id']) ? $data[$i]['item_id'] : "-";
-            $res['data'][$i]['skuid'] = isset($data[$i]['skus'][0]['SkuId']) ? $data[$i]['skus'][0]['SkuId'] : "-";
-            $res['data'][$i]['sellersku'] = isset($data[$i]['skus'][0]['SellerSku']) ? $data[$i]['skus'][0]['SellerSku'] : "-";
-            $res['data'][$i]['name'] = isset($data[$i]['attributes']['name']) ? $data[$i]['attributes']['name'] : "-";
-            $res['data'][$i]['model'] = isset($data[$i]['attributes']['model']) ? $data[$i]['attributes']['model'] : "-";
-            $res['data'][$i]['brand'] = isset($data[$i]['attributes']['brand']) ? $data[$i]['attributes']['brand'] : "-";
-            $res['data'][$i]['url'] = isset($data[$i]['skus'][0]['Url']) ? $data[$i]['skus'][0]['Url'] : "-";
-            $res['data'][$i]['quantity'] = isset($data[$i]['skus'][0]['quantity']) ? $data[$i]['skus'][0]['quantity'] : "-";
-            $res['data'][$i]['status'] = isset($data[$i]['skus'][0]['Status']) ? $data[$i]['skus'][0]['Status'] : "-";
-            $res['data'][$i]['shop_sku'] = isset($data[$i]['skus'][0]['ShopSku']) ? $data[$i]['skus'][0]['ShopSku'] : "-";
-            $res['data'][$i]['images'] = isset($data[$i]['skus'][0]['Images']) ? $data[$i]['skus'][0]['Images'] : "-";
+                $data = $executelazop['data']['products'];
+            
+                for ($i=0; $i < count($data); $i++) { 
+                    // if ($data[$i]['fee_name'] != "Payment Fee") {
+                        $setdata[$i]['nama'] = $data[$i]['attributes']['name'];
+                        $setdata[$i]['sku_lazada'] = $data[$i]['skus'][0]['ShopSku'];
+                        array_push($arr,$setdata[$i]);
+                    // }
+                }
+                $res['data'] = $arr;
+            } else {
+                $res = $executelazop;
+            }
         }
         
         return $res;
     }
-
-    // public function get_product()
-    // {
-    //     $method = 'GET';
-    //     $apiName = '/products/get';
-
-    //     $c = new LazopClient($this->apiGateway, $this->apiKey, $this->apiSecret);
-    //     $request = new LazopRequest($apiName,$method);
-
-    //     $request->addApiParam('filter','live');
-    //     // $request->addApiParam('update_before','2018-01-01T00:00:00+0800');
-    //     // $request->addApiParam('create_before','2018-01-01T00:00:00+0800');
-    //     $request->addApiParam('offset','0');
-    //     $request->addApiParam('create_after','2010-01-01T00:00:00+0800');
-    //     // $request->addApiParam('update_after','2010-01-01T00:00:00+0800');
-    //     $request->addApiParam('limit','50');
-    //     $request->addApiParam('options','1');
-    //     // $request->addApiParam('sku_seller_list',' [\"39817:01:01\", \"Apple 6S Black\"]');
-
-    //     $executelazop = json_decode($c->execute($request, $this->accessToken[0]['token']), true);
-        
-    //     return $executelazop;
-    // }
 
     public function get_transaction(Request $request)
     {
@@ -270,19 +250,10 @@ class LazopController extends Controller
     {
         $data = $this->get_product();
         $data = $data['data'];
-        for ($i=0; $i < count($data); $i++) { 
-            $dataInsert['itemid'] = $data[$i]['itemid'];
-            $dataInsert['skuid'] = $data[$i]['skuid'];
-            $dataInsert['sellersku'] = $data[$i]['sellersku'];
-            $dataInsert['name'] = $data[$i]['name'];
-            $dataInsert['model'] = $data[$i]['model'];
-            $dataInsert['brand'] = $data[$i]['brand'];
-            $dataInsert['url'] = $data[$i]['url'];
-            $dataInsert['qty'] = $data[$i]['quantity'];
-            $dataInsert['status'] = $data[$i]['status'];
-            IjcProducts::create($dataInsert);
-        }
-        return 'success';
+        // for ($i=0; $i < count($data); $i++) { 
+            Lazada::insert($data);
+        // }
+        return $data;
     }
 
     public function get_product_locally()
@@ -331,7 +302,7 @@ class LazopController extends Controller
                 for ($i=0; $i < count($data); $i++) { 
                     $data[$i]['nama_akun'] = $value['akun'];
                     $data[$i]['token'] = $value['token'];
-                    if ($data[$i]['statuses'][0] != 'canceled' && $data[$i]['statuses'][0] != 'pending' && $data[$i]['statuses'][0] != 'unpaid' && $data[$i]['statuses'][0] != 'returned' && $data[$i]['statuses'][0] != 'failed') {
+                    if ($data[$i]['statuses'][0] == 'delivered') {
                         $orders = lazadaorders::where('order_number', $data[$i]['order_number'])->first();
                         if ($orders == null) {
                             $ordersitem = $this->get_orderItem($data[$i]['order_number'],$value['token']);
